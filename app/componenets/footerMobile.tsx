0e612efe9c9ce740edd15562e6407c7c5897e79a
KBP09 <kanakbpandey2023@gmail.com>
2025-01-03 00:24:01 +0530

    LandingPage: completed
    Animations adding and emails storing left

--- app/componenets/footerMobile.tsx
+++ app/componenets/footerMobile.tsx
@@ -1,40 +1,55 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Facebook, Linkedin, Twitter, Youtube } from 'lucide-react'
 
-const Footer = () => {
+const FooterMobile = () => {
     return (
-        <footer className="hidden lg:flex w-full flex-col sm:flex-row h-[600px]">
-            <div className="bg-black text-white py-12 w-[50%] flex items-center justify-center">
-                <div className="container mx-auto px-6 flex flex-col justify-between h-full">
-                    <div className="space-y-4 flex flex-col items-center justify-center flex-grow">
+        <footer className="lg:hidden flex flex-col w-full h-[600px]">
+            <div className="bg-[#1A1A1D] text-white py-12 flex items-center justify-center">
+                <div className="container flex flex-col justify-between items-center h-full">
+                    <div className="flex items-center justify-center flex-grow mb-4">
                         <Image
                             src="/logo.svg"
                             alt="Logo"
-                            width={100}
-                            height={50}
-                            className="w-[80px] sm:w-[100px]"
+                            width={80}
+                            height={40}
+                            className="w-[70px] sm:w-[80px]"
                         />
-                        <h2 className="text-5xl font-semibold">DecentInvest</h2>
-                        <p className="text-[#E7E7E7] text-lg xl:text-2xl leading-none">
-                            Invest Decently, Invest Decentrally
-                        </p>
+                        <h2 className="text-3xl font-semibold">DecentInvest</h2>
                     </div>
-
-                    <p className="text-[#E7E7E7] text-md leading-none self-center mt-auto">
-                        © 2024 Brand, Inc. • Privacy • Terms
+                    <p className="text-[#E7E7E7] text-xl xl:text-2xl leading-none">
+                        Invest Decently, Invest Decentrally
                     </p>
+                    <div className="mt-16 flex flex-col w-full px-8">
+                        <h3 className="text-[#E7E7E7] font-medium mb-2 text-left md:text-[18px] lg:text-[25px]">Subscribe to our newsletter</h3>
+                        <div className="flex items-center w-full">
+                            <input
+                                type="email"
+                                placeholder="Enter your Email Address"
+                                className="bg-[#E7E7E7] px-3 py-2 text-black rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500 flex-grow h-[35px]"
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="bg-[#FFB200] text-black py-1 sm:py-2  rounded-xl hover:bg-yellow-400 mt-4 w-[120px] sm:w-[150px] ml-auto"
+                        >
+                            Submit
+                        </button>
+                    </div>
                 </div>
+
             </div>
 
-            <div className="bg-[#1A1A1D] text-white w-full flex items-center justify-center flex-col">
-                <div className="grid grid-cols-3 gap-2 md:text-[18px] lg:text-[25px] w-full">
+            <div className="bg-black text-white w-full flex items-center justify-center flex-col">
+                <div className="grid grid-cols-2 gap-2 md:text-[18px] lg:text-[25px] w-full mt-8">
                     <Link href="/" className="text-[#E7E7E7] hover:text-gray-300 text-center">Home</Link>
                     <Link href="/about" className="text-[#E7E7E7] hover:text-gray-300 text-center">Startups</Link>
-                    <p className="text-[#E7E7E7] text-center">Contact Us:</p>
                     <Link href="/newsletter" className="text-[#E7E7E7] hover:text-gray-300 text-center">Newsletter</Link>
                     <Link href="/startups" className="text-[#E7E7E7] hover:text-gray-300 text-center">About</Link>
-                    <div className="flex items-center justify-center gap-2">
+                </div>
+                <p className="text-[#E7E7E7] text-center mt-8">Contact Us:</p>
+                <div className="flex items-center justify-center gap-2 mt-2">
                         <Link href="#" className="hover:text-gray-300">
                             <Image
                                 src="/twitter.svg"
@@ -72,31 +87,13 @@ const Footer = () => {
                             />
                         </Link>
                     </div>
-                </div>
-
-                <div className="mt-16 flex items-center justify-center w-full md:mr-16 lg:mr-0">
-                    <div className="flex flex-col w-[60%]">
-                        <h3 className="text-[#E7E7E7] font-medium mb-2 text-left md:text-[18px] lg:text-[25px]">Subscribe to our newsletter</h3>
-                        <div className="flex items-center w-full">
-                            <input
-                                type="email"
-                                placeholder="Enter your Email Address"
-                                className="bg-[#E7E7E7] px-3 py-2 text-black rounded-md focus:outline-none focus:ring-2 focus:ring-yellow-500 flex-grow"
-                            />
-                            <button
-                                type="submit"
-                                className="bg-[#FFB200] text-black px-8 py-2 ml-2 rounded-xl hover:bg-yellow-400"
-                            >
-                                Submit
-                            </button>
-                        </div>
-                    </div>
-                </div>
-
+                    <p className="text-[#E7E7E7] text-sm leading-none self-center mt-8">
+                        © 2024 Brand, Inc. • Privacy • Terms
+                    </p>
             </div>
 
         </footer>
     )
 }
 
-export default Footer
+export default FooterMobile;
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Navbar from "./componenets/navbar";
 import Footer from "./componenets/footer";
+import FooterMobile from "./componenets/footerMobile";
 import myImage from "../public/Bitcoin.svg"
 
 
@@ -10,17 +11,17 @@ export default function Home() {
   return (
     <>
       <Navbar />
-      <div className="h-screen w-full bg-cover bg-center relative flex items-center justify-center flex-col">
+      <div className="h-screen sm:h-screen w-full bg-cover bg-center relative flex items-center justify-center flex-col">
         <div
-          className="absolute inset-0 bg-cover bg-center bg-[url('../public/homebg.svg')] blur-sm z-0"
+          className="absolute inset-0 bg-cover bg-center bg-[url('../public/homebg.svg')] "
         />
         <div
-          className="mt-16 h-[80vh] w-[80vw] bg-[#D9D9D91A] border-4 rounded-2xl overflow-hidden flex items-center justify-center flex-col border-gradient relative z-10"
+          className="mt-16 h-[80vh] w-[80vw] bg-[#D9D9D91A] border-4 rounded-[48px] border-[golden] overflow-hidden flex items-center justify-center flex-col relative z-10 backdrop-blur-lg"
         >
           <h1 className="text-[40px] sm:text-[100px] xl:text-[130px] font-bold mb-8 sm:mb-0">DecentInvest</h1>
           <h2 className="text-[18px] xl:text-[40px] mb-8 sm:mb-0">Invest Decently, Invest Decentrally</h2>
           <h2 className=" text-[12px] xl:text-[30px]  mb-8 sm:mb-0">Secure, transparent, and fair investments</h2>
-          <button className="bg-[#FFB200] text-black font-medium  w-[147px] h-[35px] sm:h-[65px] sm:w-[281px] rounded-2xl hover:bg-yellow-500 transition-colors duration-300 text-[18px] mt-8 ">
+          <button className="bg-[#FFB200] text-black font-medium  w-[147px] h-[35px] sm:h-[54px] sm:w-[220px] rounded-2xl hover:bg-yellow-500 transition-colors duration-300 text-[25px] mt-8 ">
             Get Started
           </button>
         </div>
@@ -220,6 +221,7 @@ export default function Home() {
 
       </div>
       <Footer/>
+      <FooterMobile/>
     </>
   );
 }
