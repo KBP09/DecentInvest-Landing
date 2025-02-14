7b2e4eda0345fcda4f5b929fba47d2f0689e6e75
KBP09 <kanakbpandey2023@gmail.com>
2025-02-14 16:30:06 +0530

    touch ups
    Signed-off-by: KBP09 <kanakbpandey2023@gmail.com>

--- app/componenets/heroSection.tsx
+++ app/componenets/heroSection.tsx
@@ -71,16 +71,14 @@ const HeroSection = () => {
         className="mt-24 h-[80vh] w-[80vw] flex flex-col items-center justify-center relative z-10 rounded-[48px] shadow-lg p-6 sm:p-10 border-4 border-white backdrop-blur-md"
       >
         <div className="text-center">
-          <h1 className="text-white text-[42px] sm:text-[64px] md:text-[100px] xl:text-[120px] font-extrabold mb-2 sm:mt-0 leading-tight tracking-wide drop-shadow-[0_0_20px_rgba(255,255,255,0.5)]">
+          <h1 className="text-white text-[35px] sm:text-[64px] md:text-[100px] xl:text-[100px] font-extrabold mb-2 sm:mt-0 leading-tight tracking-wide drop-shadow-[0_0_20px_rgba(255,255,255,0.5)]">
             DecentInvest
           </h1>
           <div className="flex flex-col gap-3">
             <h2 className="text-[#FFD700] text-[20px] md:text-[30px] xl:text-[40px] font-semibold flex items-center justify-center gap-2">
-              <FaChartLine className="text-yellow-400" />
               Invest Decently, Invest Decentrally
             </h2>
             <h2 className="text-gray-300 text-[14px] md:text-[18px] xl:text-[25px] italic flex items-center justify-center gap-2">
-              <FaLock className="text-green-400" />
               Secure, Transparent, and Fair Investments
             </h2>
           </div>
